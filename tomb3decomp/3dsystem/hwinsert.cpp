b6b043896a5a0f37945aa30366f4fd843e2a3dba
Troye <woopstombraider@gmail.com>
2022-05-11 14:40:46 +0300

    SubdivideEdge;

--- tomb3decomp/3dsystem/hwinsert.cpp
+++ tomb3decomp/3dsystem/hwinsert.cpp
@@ -41,7 +41,56 @@ void HWI_InsertTrans8_Sorted(PHD_VBUF* buf, short shade)
 	}
 }
 
+void SubdivideEdge(PHD_VBUF* v0, PHD_VBUF* v1, PHD_VBUF* dest)
+{
+	float zv;
+	short r, g, b, r0, g0, b0, r1, g1, b1;
+
+	dest->xv = (v1->xv + v0->xv) * 0.5F;
+	dest->yv = (v0->yv + v1->yv) * 0.5F;
+	dest->zv = (v0->zv + v1->zv) * 0.5F;
+
+	if (dest->zv <= phd_znear)
+		dest->clip = -128;
+	else
+	{
+		zv = f_persp / dest->zv;
+		dest->xs = dest->xv * zv + f_centerx;
+		dest->ys = dest->yv * zv + f_centery;
+		dest->ooz = zv * f_oneopersp;
+		dest->clip = 0;
+
+		if (dest->xs < phd_leftfloat)
+			dest->clip++;
+		else if (dest->xs > phd_rightfloat)
+			dest->clip += 2;
+
+		if (dest->ys < phd_topfloat)
+			dest->clip += 4;
+		else if (dest->ys > phd_bottomfloat)
+			dest->clip += 8;
+	}
+
+	r0 = (v0->g >> 10) & 0x1F;
+	r1 = (v1->g >> 10) & 0x1F;
+
+	g0 = (v0->g >> 5) & 0x1F;
+	g1 = (v1->g >> 5) & 0x1F;
+
+	b0 = v0->g & 0x1F;
+	b1 = v1->g & 0x1F;
+
+	r = (r0 + r1) >> 1;
+	g = (g0 + g1) >> 1;
+	b = (b0 + b1) >> 1;
+	dest->g = short(r << 10 | g << 5 | b);
+
+	dest->u = (v0->u + v1->u) >> 1;
+	dest->v = (v0->v + v1->v) >> 1;
+}
+
 void inject_hwinsert(bool replace)
 {
 	INJECT(0x0040A850, HWI_InsertTrans8_Sorted, replace);
+	INJECT(0x00406880, SubdivideEdge, replace);
 }
--- tomb3decomp/3dsystem/hwinsert.h
+++ tomb3decomp/3dsystem/hwinsert.h
@@ -4,6 +4,7 @@
 void inject_hwinsert(bool replace);
 
 void HWI_InsertTrans8_Sorted(PHD_VBUF* buf, short shade);
+void SubdivideEdge(PHD_VBUF* v0, PHD_VBUF* v1, PHD_VBUF* dest);
 
 #define XYClipper	( (long(__cdecl*)(long, VERTEX_INFO*)) 0x0040CA50 )
 #define HWI_InsertPoly_Gouraud	( (void(__cdecl*)(long, float, long, long, long, long)) 0x00408940 )
