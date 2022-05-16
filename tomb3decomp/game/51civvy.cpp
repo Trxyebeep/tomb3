8978634f3d9bb8a322ac5ef2faff9ba7c48e4701
Troye <woopstombraider@gmail.com>
2022-05-16 05:37:05 +0300

    ControlElectricFence;

--- tomb3decomp/game/51civvy.cpp
+++ tomb3decomp/game/51civvy.cpp
@@ -2,6 +2,8 @@
 #include "51civvy.h"
 #include "effect2.h"
 #include "../specific/function_stubs.h"
+#include "control.h"
+#include "gameflow.h"
 
 static void TriggerFenceSparks(long x, long y, long z, long kill)
 {
@@ -40,7 +42,147 @@ static void TriggerFenceSparks(long x, long y, long z, long kill)
 	sptr->Gravity = (GetRandomControl() & 0xF) + 16;
 }
 
+void ControlElectricFence(short item_number)
+{
+	ITEM_INFO* item;
+	long dx, dz, x, z, ex, ey, ez, xa, za, xs, zs, num, num2, oex, oez;
+
+	item = &items[item_number];
+
+	if (!TriggerActive(item))
+		return;
+
+	dx = lara_item->pos.x_pos - item->pos.x_pos;
+	dz = lara_item->pos.z_pos - item->pos.z_pos;
+
+	if (dx < -20480 || dx > 20480 || dz < -20480 || dz > 20480)
+		return;
+
+	if (!item->pos.y_rot)
+	{
+		x = item->pos.x_pos + 512;
+		z = item->pos.z_pos + 512;
+		ex = x - 992;
+		ez = item->pos.z_pos + 256;
+		xa = 0x7FF;
+		za = 0;
+		xs = 1056;
+		zs = 128;
+	}
+	else if (item->pos.y_rot == -0x4000)
+	{
+		x = item->pos.x_pos - 512;
+		z = item->pos.z_pos + 512;
+		ex = x + 256;
+		ez = z - 992;
+		xa = 0;
+		za = 0x7FF;
+		xs = 128;
+		zs = 1056;
+	}
+	else if (item->pos.y_rot == -0x8000)
+	{
+		x = item->pos.x_pos - 512;
+		z = item->pos.z_pos - 512;
+		ex = x - 992;
+		ez = item->pos.z_pos - 256;
+		xa = 2047;
+		za = 0;
+		xs = 1056;
+		zs = 128;
+	}
+	else if (item->pos.y_rot == 0x4000)
+	{
+		x = item->pos.x_pos + 512;
+		z = item->pos.z_pos - 512;
+		ex = x - 256;
+		ez = z - 992;
+		xa = 0;
+		za = 2047;
+		xs = 128;
+		zs = 1056;
+	}
+	else
+	{
+		x = 0;
+		z = 0;
+		ex = 0;
+		ez = 0;
+		xa = 0;
+		za = 0;
+		xs = 0;
+		zs = 0;
+	}
+
+	if (!(GetRandomControl() & 0x3F))
+	{
+		if (xa)
+			ex += GetRandomControl() & xa;
+		else
+			ez += GetRandomControl() & za;
+
+		if (CurrentLevel == LV_OFFICE)
+			ey = item->pos.y_pos - (GetRandomControl() & 0x1F);
+		else
+			ey = item->pos.y_pos - (GetRandomControl() & 0x7FF) - (GetRandomControl() & 0x3FF);
+
+		num = (GetRandomControl() & 3) + 3;
+
+		for (int i = 0; i < num; i++)
+		{
+			TriggerFenceSparks(ex, ey, ez, 0);
+
+			if (xa)
+				ex += (xa & GetRandomControl() & 7) - 4;
+			else
+				ez += (za & GetRandomControl() & 7) - 4;
+
+			ey += (GetRandomControl() & 7) - 4;
+		}
+	}
+
+	if (lara.electric || CurrentLevel == LV_OFFICE ||
+		lara_item->pos.x_pos < x - xs || lara_item->pos.x_pos > x + xs ||
+		lara_item->pos.z_pos < z - zs || lara_item->pos.z_pos > z + zs ||
+		lara_item->pos.y_pos > item->pos.y_pos + 32 || lara_item->pos.y_pos < item->pos.y_pos - 3072)
+		return;
+
+	oex = ex;
+	oez = ez;
+	num = (GetRandomControl() & 0xF) + 3;
+
+	for (int i = 0; i < num; i++)
+	{
+		if (xa)
+			ex = (GetRandomControl() & 0x1FF) + lara_item->pos.x_pos - 256;
+		else
+			ez = (GetRandomControl() & 0x1FF) + lara_item->pos.z_pos - 256;
+
+		ey = lara_item->pos.y_pos - GetRandomControl() % 768;
+		num2 = (GetRandomControl() & 3) + 6;
+
+		for (int j = 0; j < num2; j++)
+		{
+			TriggerFenceSparks(ex, ey, ez, 1);
+
+			if (xa)
+				ex += (xa & GetRandomControl() & 7) - 4;
+			else
+				ez += (za & GetRandomControl() & 7) - 4;
+
+			ey += (GetRandomControl() & 7) - 4;
+		}
+
+		ex = oex;
+		ez = oez;
+	}
+
+	lara.electric = 1;
+	lara_item->hit_points = 0;
+}
+
 void inject_civvy(bool replace)
 {
 	INJECT(0x0040F080, TriggerFenceSparks, replace);
+	INJECT(0x0040ECA0, ControlElectricFence, replace);
 }
--- tomb3decomp/game/51civvy.h
+++ tomb3decomp/game/51civvy.h
@@ -2,3 +2,5 @@
 #include "../global/vars.h"
 
 void inject_civvy(bool replace);
+
+void ControlElectricFence(short item_number);
