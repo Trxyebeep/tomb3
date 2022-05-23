c306f31f6fd465040f2bb58c0548eadde7ae61ff
Troye <woopstombraider@gmail.com>
2022-05-23 15:39:38 -0500

    CanUseGun

--- tomb3decomp/game/biggun.cpp
+++ tomb3decomp/game/biggun.cpp
@@ -17,7 +17,25 @@ void BigGunInitialise(short item_number)
 	gun->StartRotY = item->pos.y_rot;
 }
 
+static long CanUseGun(ITEM_INFO* gun, ITEM_INFO* l)
+{
+	long dx, dz, dist;
+
+	if (!(input & IN_ACTION) || lara.gun_status != LG_ARMLESS || l->gravity_status)
+		return 0;
+
+	dx = l->pos.x_pos - gun->pos.x_pos;
+	dz = l->pos.z_pos - gun->pos.z_pos;
+	dist = SQUARE(dx) + SQUARE(dz);
+
+	if (dist > 30000)
+		return 0;
+
+	return 1;
+}
+
 void inject_biggun(bool replace)
 {
 	INJECT(0x00410D00, BigGunInitialise, replace);
+	INJECT(0x00410E60, CanUseGun, replace);
 }
