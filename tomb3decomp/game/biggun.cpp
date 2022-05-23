dd30565e5af6e3c017c848779b524bfc799dca9e
Troye <woopstombraider@gmail.com>
2022-05-23 16:05:39 -0500

    BigGunDraw

--- tomb3decomp/game/biggun.cpp
+++ tomb3decomp/game/biggun.cpp
@@ -5,6 +5,9 @@
 #include "laraanim.h"
 #include "objects.h"
 #include "collide.h"
+#include "draw.h"
+#include "../3dsystem/3d_gen.h"
+#include "../specific/output.h"
 
 void BigGunInitialise(short item_number)
 {
@@ -75,9 +78,73 @@ void BigGunCollision(short item_number, ITEM_INFO* l, COLL_INFO* coll)
 		ObjectCollision(item_number, l, coll);
 }
 
+void BigGunDraw(ITEM_INFO* item)
+{
+	OBJECT_INFO* obj;
+	short** meshpp;
+	long* bone;
+	short* frm[2];
+	short* rot1;
+	short* rot2;
+	long frac, rate, clip;
+
+	frac = GetFrames(item, frm, &rate);
+	obj = &objects[item->object_number];
+
+	phd_PushMatrix();
+	phd_TranslateAbs(item->pos.x_pos, item->pos.y_pos, item->pos.z_pos);
+	phd_RotYXZ(item->pos.y_rot, item->pos.x_rot, item->pos.z_rot);
+	clip = S_GetObjectBounds(frm[0]);
+
+	if (clip)
+	{
+		CalculateObjectLighting(item, frm[0]);
+		meshpp = &meshes[obj->mesh_index];
+		bone = &bones[obj->bone_index];
+
+		if (frac)
+		{
+			InitInterpolate(frac, rate);
+			rot1 = frm[0] + 9;
+			rot2 = frm[1] + 9;
+			phd_TranslateRel_ID(frm[0][6], frm[0][7], frm[0][8], frm[1][6], frm[1][7], frm[1][8]);
+
+			gar_RotYXZsuperpack_I(&rot1, &rot2, 0);
+			phd_PutPolygons_I(*meshpp++, clip);
+
+			phd_TranslateRel_I(bone[1], bone[2], bone[3]);
+			gar_RotYXZsuperpack_I(&rot1, &rot2, 0);
+			phd_PutPolygons_I(*meshpp++, clip);
+
+			phd_TranslateRel_I(bone[5], bone[6], bone[7]);
+			gar_RotYXZsuperpack_I(&rot1, &rot2, 0);
+			phd_PutPolygons_I(*meshpp++, clip);
+		}
+		else
+		{
+			phd_TranslateRel(frm[0][6], frm[0][7], frm[0][8]);
+			rot1 = frm[0] + 9;
+
+			gar_RotYXZsuperpack(&rot1, 0);
+			phd_PutPolygons(*meshpp++, clip);
+
+			phd_TranslateRel(bone[1], bone[2], bone[3]);
+			gar_RotYXZsuperpack(&rot1, 0);
+			phd_PutPolygons(*meshpp++, clip);
+
+			phd_TranslateRel(bone[5], bone[6], bone[7]);
+			gar_RotYXZsuperpack(&rot1, 0);
+			phd_PutPolygons(*meshpp++, clip);
+		}
+	}
+
+	phd_PopMatrix();
+}
+
 void inject_biggun(bool replace)
 {
 	INJECT(0x00410D00, BigGunInitialise, replace);
 	INJECT(0x00410E60, CanUseGun, replace);
 	INJECT(0x00410D50, BigGunCollision, replace);
+	INJECT(0x00410EC0, BigGunDraw, replace);
 }
--- tomb3decomp/game/biggun.h
+++ tomb3decomp/game/biggun.h
@@ -5,5 +5,6 @@ void inject_biggun(bool replace);
 
 void BigGunInitialise(short item_number);
 void BigGunCollision(short item_number, ITEM_INFO* l, COLL_INFO* coll);
+void BigGunDraw(ITEM_INFO* item);
 
 #define BigGunControl	( (long(__cdecl*)(COLL_INFO*)) 0x00411100 )
