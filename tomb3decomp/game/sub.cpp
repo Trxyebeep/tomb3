5facb7ab332209a4cc5e8dd1f84a73d208cd962a
Troye <woopstombraider@gmail.com>
2022-05-28 17:02:29 -0500

    GetOnSub

--- tomb3decomp/game/sub.cpp
+++ tomb3decomp/game/sub.cpp
@@ -1,6 +1,7 @@
 #include "../tomb3/pch.h"
 #include "sub.h"
 #include "../specific/init.h"
+#include "control.h"
 
 void SubInitialise(short item_number)
 {
@@ -22,7 +23,41 @@ void SubInitialise(short item_number)
 	}
 }
 
+static long GetOnSub(short item_number, COLL_INFO* coll)
+{
+	ITEM_INFO* item;
+	FLOOR_INFO* floor;
+	long dx, dy, dz, dist, h;
+	short room_number;
+
+	if (!(input & IN_ACTION) || lara.gun_status != LG_ARMLESS || lara_item->gravity_status)
+		return 0;
+
+	item = &items[item_number];
+	dy = lara_item->pos.y_pos - item->pos.y_pos - 128;
+
+	if (ABS(dy) > 256)
+		return 0;
+
+	dx = lara_item->pos.x_pos - item->pos.x_pos;
+	dz = lara_item->pos.z_pos - item->pos.z_pos;
+	dist = SQUARE(dx) + SQUARE(dz);
+
+	if (dist > 0x40000)
+		return 0;
+
+	room_number = item->room_number;
+	floor = GetFloor(item->pos.x_pos, item->pos.y_pos, item->pos.z_pos, &room_number);
+	h = GetHeight(floor, item->pos.x_pos, item->pos.y_pos, item->pos.z_pos);
+
+	if (h < -32000)
+		return 0;
+
+	return 1;
+}
+
 void inject_sub(bool replace)
 {
 	INJECT(0x004685C0, SubInitialise, replace);
+	INJECT(0x00468780, GetOnSub, replace);
 }
