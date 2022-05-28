9e12e2ce8cd1cc00caebc5886090e1eb600723e4
Troye <woopstombraider@gmail.com>
2022-05-28 01:54:19 -0500

    fix my mist fix

--- tomb3decomp/game/kayak.cpp
+++ tomb3decomp/game/kayak.cpp
@@ -1230,6 +1230,9 @@ long KayakControl()
 	ITEM_INFO* item;
 	KAYAKINFO* kayak;
 	FLOOR_INFO* floor;
+#ifdef TROYESTUFF
+	PHD_VECTOR pos;
+#endif
 	long ofs, h, wh, damage, x, y, z;
 	short room_number;
 	static short MistXPos[10] = { 32, 96, 170, 220, 300, 400, 400, 300,  200, 64 };
@@ -1318,10 +1321,10 @@ long KayakControl()
 			phd_PushUnitMatrix();
 			phd_RotYXZ(item->pos.y_rot, item->pos.x_rot, item->pos.z_rot);
 #ifdef TROYESTUFF
-			x = (phd_mxptr[M00] * x + phd_mxptr[M01] * y + phd_mxptr[M02] * z) >> W2V_SHIFT;	//fixes mist being all over the place
-			y = (phd_mxptr[M10] * x + phd_mxptr[M11] * y + phd_mxptr[M12] * z) >> W2V_SHIFT;
-			z = (phd_mxptr[M20] * x + phd_mxptr[M21] * y + phd_mxptr[M22] * z) >> W2V_SHIFT;
-			TriggerRapidsMist(item->pos.x_pos + x, item->pos.y_pos + y, item->pos.z_pos + z);
+			pos.x = (phd_mxptr[M00] * x + phd_mxptr[M01] * y + phd_mxptr[M02] * z) >> W2V_SHIFT;	//fixes mist being all over the place
+			pos.y = (phd_mxptr[M10] * x + phd_mxptr[M11] * y + phd_mxptr[M12] * z) >> W2V_SHIFT;
+			pos.z = (phd_mxptr[M20] * x + phd_mxptr[M21] * y + phd_mxptr[M22] * z) >> W2V_SHIFT;
+			TriggerRapidsMist(item->pos.x_pos + pos.x, item->pos.y_pos + pos.y, item->pos.z_pos + pos.z);
 #else
 			x = item->pos.x_pos + ((phd_mxptr[M00] * x + phd_mxptr[M01] * y + phd_mxptr[M02] * z) >> W2V_SHIFT);
 			y = item->pos.y_pos + ((phd_mxptr[M10] * x + phd_mxptr[M11] * y + phd_mxptr[M12] * z) >> W2V_SHIFT);
