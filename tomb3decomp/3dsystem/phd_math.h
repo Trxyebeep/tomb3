54251ce5ab25761cac8f066532fa434b58ea9dce
Troye <woopstombraider@gmail.com>
2022-06-03 20:28:49 -0500

    phd_sin, phd_cos;

--- tomb3decomp/3dsystem/phd_math.h
+++ tomb3decomp/3dsystem/phd_math.h
@@ -5,6 +5,5 @@ void inject_phdmath(bool replace);
 
 ulong __fastcall phd_sqrt(ulong num);
 long __fastcall phd_atan(long x, long y);
-
-#define phd_sin	( (long(__fastcall*)(long)) 0x004B4C5E )
-#define phd_cos	( (long(__fastcall*)(long)) 0x004B4C58 )
+long __fastcall phd_sin(long angle);
+long __fastcall phd_cos(long angle);
