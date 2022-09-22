1c7d4505d770695d39ae2aaa5411ccf51306aa93
Troye <woopstombraider@gmail.com>
2022-09-22 07:13:56 -0500

    DrawPictureAlpha

--- tomb3decomp/specific/picture.h
+++ tomb3decomp/specific/picture.h
@@ -6,7 +6,7 @@ void inject_picture(bool replace);
 void CrossFadePicture();
 void S_FadePicture();
 void DrawTile(long x, long y, long w, long h, long tpage, long tU, long tV, long tW, long tH, long c0, long c1, long c2, long c3, float z);
+void DrawPictureAlpha(long col, long* indices, float z);
 
-#define DrawPictureAlpha	( (void(__cdecl*)(long, long*, float)) 0x0048ADA0 )
 #define FreePictureTextures	( (void(__cdecl*)(long*)) 0x0048B190 )
 #define TRDrawPicture	( (void(__cdecl*)(long, long*, float)) 0x0048BA30 )
