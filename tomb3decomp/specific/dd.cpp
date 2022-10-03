0f55940e5c3cdde868ee2aa0a248f7dd277cfaf5
Troye <woopstombraider@gmail.com>
2022-10-03 18:49:22 -0500

    DD_CreateSurface; DD_EnsureSurfaceAvailable; DD_ClearSurface;

--- tomb3decomp/specific/dd.cpp
+++ tomb3decomp/specific/dd.cpp
@@ -57,9 +57,57 @@ HRESULT DD_UnlockSurface(LPDIRECTDRAWSURFACE3 surf, DDSURFACEDESC& desc)
 	return surf->Unlock(desc.lpSurface);
 }
 
+HRESULT DD_CreateSurface(DDSURFACEDESC& desc, LPDIRECTDRAWSURFACE3& surf)
+{
+	HRESULT res;
+	LPDIRECTDRAWSURFACE ps;
+
+	res = App.lpDD->CreateSurface(&desc, &ps, 0);
+
+	if (FAILED(res))
+		return res;
+
+	res = ps->QueryInterface(IID_IDirectDrawSurface3, (LPVOID*)&surf);
+	ps->Release();
+	return res;
+}
+
+HRESULT DD_EnsureSurfaceAvailable(LPDIRECTDRAWSURFACE3 surf, LPDIRECTDRAWSURFACE3 tSurf, bool clear)
+{
+	HRESULT res;
+
+	res = surf->IsLost();
+
+	if (res == DDERR_SURFACELOST)
+	{
+		if (!tSurf)
+			tSurf = surf;
+
+		res = surf->Restore();
+
+		if (SUCCEEDED(res) && clear)
+			DD_ClearSurface(surf, 0, 0);
+	}
+
+	return res;
+}
+
+bool DD_ClearSurface(LPDIRECTDRAWSURFACE3 surf, LPRECT rect, ulong col)
+{
+	DDBLTFX blt;
+
+	memset(&blt, 0, sizeof(DDBLTFX));
+	blt.dwSize = sizeof(DDBLTFX);
+	blt.dwFillColor = col;
+	return SUCCEEDED(surf->Blt(rect, 0, 0, DDBLT_WAIT | DDBLT_COLORFILL, &blt));
+}
+
 void inject_dd(bool replace)
 {
 	INJECT(0x004B3830, DD_SpinMessageLoop, replace);
 	INJECT(0x004B3900, DD_LockSurface, replace);
 	INJECT(0x004B3930, DD_UnlockSurface, replace);
+	INJECT(0x004B3950, DD_CreateSurface, replace);
+	INJECT(0x004B39A0, DD_EnsureSurfaceAvailable, replace);
+	INJECT(0x004B39F0, DD_ClearSurface, replace);
 }
--- tomb3decomp/specific/dd.h
+++ tomb3decomp/specific/dd.h
@@ -6,3 +6,6 @@ void inject_dd(bool replace);
 bool DD_SpinMessageLoop(bool wait);
 HRESULT DD_LockSurface(LPDIRECTDRAWSURFACE3 surf, DDSURFACEDESC& desc, ulong flags);
 HRESULT DD_UnlockSurface(LPDIRECTDRAWSURFACE3 surf, DDSURFACEDESC& desc);
+HRESULT DD_CreateSurface(DDSURFACEDESC& desc, LPDIRECTDRAWSURFACE3& surf);
+HRESULT DD_EnsureSurfaceAvailable(LPDIRECTDRAWSURFACE3 surf, LPDIRECTDRAWSURFACE3 tSurf, bool clear);
+bool DD_ClearSurface(LPDIRECTDRAWSURFACE3 surf, LPRECT rect, ulong col);
