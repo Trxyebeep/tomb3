ed0e0db77720bf5f749f28a468809b48a2b973a8
Troye <woopstombraider@gmail.com>
2022-10-04 18:50:19 -0500

    SelectMeshes

--- tomb3decomp/game/inventry.cpp
+++ tomb3decomp/game/inventry.cpp
@@ -1,5 +1,6 @@
 #include "../tomb3/pch.h"
 #include "inventry.h"
+#include "objects.h"
 
 long GetDebouncedInput(long in)
 {
@@ -11,7 +12,38 @@ long GetDebouncedInput(long in)
 	return i & in;
 }
 
+void SelectMeshes(INVENTORY_ITEM* item)
+{
+	if (item->object_number == PASSPORT_OPTION)
+	{
+		if (item->current_frame < 4)
+			item->drawn_meshes = 23;
+		else if (item->current_frame <= 16)
+			item->drawn_meshes = 87;
+		else if (item->current_frame < 19)
+			item->drawn_meshes = 95;
+		else if (item->current_frame == 19)
+			item->drawn_meshes = 91;
+		else if (item->current_frame < 24)
+			item->drawn_meshes = 123;
+		else if (item->current_frame < 29)
+			item->drawn_meshes = 59;
+		else if (item->current_frame == 29)
+			item->drawn_meshes = 19;
+	}
+	else if (item->object_number == MAP_OPTION)
+	{
+		if (!item->current_frame || item->current_frame >= 18)
+			item->drawn_meshes = item->which_meshes;
+		else
+			item->drawn_meshes = -1;
+	}
+	else if (item->object_number != GAMMA_OPTION)
+		item->drawn_meshes = -1;
+}
+
 void inject_inventry(bool replace)
 {
 	INJECT(0x00436FA0, GetDebouncedInput, replace);
+	INJECT(0x004369C0, SelectMeshes, replace);
 }
