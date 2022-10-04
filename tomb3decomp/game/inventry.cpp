8b8e4b98793c851c1a9913607adccda82c210c84
Troye <woopstombraider@gmail.com>
2022-10-04 18:55:07 -0500

    AnimateInventoryItem

--- tomb3decomp/game/inventry.cpp
+++ tomb3decomp/game/inventry.cpp
@@ -42,8 +42,34 @@ void SelectMeshes(INVENTORY_ITEM* item)
 		item->drawn_meshes = -1;
 }
 
+long AnimateInventoryItem(INVENTORY_ITEM* item)
+{
+	if (item->current_frame == item->goal_frame)
+	{
+		SelectMeshes(item);
+		return 0;
+	}
+
+	if (item->anim_count)
+		item->anim_count--;
+	else
+	{
+		item->anim_count = item->anim_speed;
+		item->current_frame += item->anim_direction;
+
+		if (item->current_frame >= item->frames_total)
+			item->current_frame = 0;
+		else if (item->current_frame < 0)
+			item->current_frame = item->frames_total - 1;
+	}
+
+	SelectMeshes(item);
+	return 1;
+}
+
 void inject_inventry(bool replace)
 {
 	INJECT(0x00436FA0, GetDebouncedInput, replace);
 	INJECT(0x004369C0, SelectMeshes, replace);
+	INJECT(0x00436A50, AnimateInventoryItem, replace);
 }
