58c911ca841752017765e3eddeb1bc9b3a0c4a1d
Troye <woopstombraider@gmail.com>
2022-10-17 01:46:57 -0500

    UT_CenterWindow

--- tomb3decomp/specific/utils.cpp
+++ tomb3decomp/specific/utils.cpp
@@ -32,8 +32,22 @@ void UT_InitAccurateTimer()
 	}
 }
 
+void UT_CenterWindow(HWND hwnd)
+{
+	RECT window;
+	RECT area;
+
+	SystemParametersInfo(SPI_GETWORKAREA, 0, &area, 0);
+	GetWindowRect(hwnd, &window);
+	SetWindowPos(hwnd, 0,
+		((area.left + area.right) >> 1) - ((window.right - window.left) >> 1),
+		((area.top + area.bottom) >> 1) - ((window.bottom - window.top) >> 1),
+		-1, -1, SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
+}
+
 void inject_utils(bool replace)
 {
 	INJECT(0x0048E440, UT_GetAccurateTimer, replace);
 	INJECT(0x0048E3E0, UT_InitAccurateTimer, replace);
+	INJECT(0x0048E490, UT_CenterWindow, replace);
 }
--- tomb3decomp/specific/utils.h
+++ tomb3decomp/specific/utils.h
@@ -5,5 +5,6 @@ void inject_utils(bool replace);
 
 double UT_GetAccurateTimer();
 void UT_InitAccurateTimer();
+void UT_CenterWindow(HWND hwnd);
 
 #define UT_OKCancelBox	( (bool(__cdecl*)(char*, HWND)) 0x0048E530 )
