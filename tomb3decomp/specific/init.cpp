cb8eaec69d635a6a46db0ca1393ce9a79d03666d
Troye <woopstombraider@gmail.com>
2022-10-18 02:56:28 -0500

    GetRandom; init_water_table;

--- tomb3decomp/specific/init.cpp
+++ tomb3decomp/specific/init.cpp
@@ -53,8 +53,78 @@ void CalculateWibbleTable()
 	}
 }
 
+ushort GetRandom(WATERTAB* wt, long lp)
+{
+	long loop;
+	ushort ret;
+
+	do
+	{
+		ret = rand() & 0xFC;
+
+		for (loop = 0; loop < lp; loop++)
+			if (wt[loop].random == ret)
+				break;
+
+	} while (loop != lp);
+
+	return ret;
+}
+
+void init_water_table()
+{
+	short sin;
+	static short water_shimmer[4] = { 31, 63, 95, 127 };
+	static short water_choppy[4] = { 16, 53, 90, 127 };
+	static uchar water_abs[4] = { 4, 8, 12, 16 };
+
+	srand(121197);
+
+	for (int i = 0; i < 64; i++)
+	{
+		sin = rcossin_tbl[i << 7];
+		WaterTable[0][i].shimmer = (63 * sin) >> 15;
+		WaterTable[0][i].choppy = (16 * sin) >> 12;
+		WaterTable[0][i].random = (uchar)GetRandom(&WaterTable[0][0], i);
+		WaterTable[0][i].abs = 0;
+
+		WaterTable[1][i].shimmer = (32 * sin) >> 15;
+		WaterTable[1][i].choppy = 0;
+		WaterTable[1][i].random = (uchar)GetRandom(&WaterTable[1][0], i);
+		WaterTable[1][i].abs = -3;
+
+		WaterTable[2][i].shimmer = (64 * sin) >> 15;
+		WaterTable[2][i].choppy = 0;
+		WaterTable[2][i].random = (uchar)GetRandom(&WaterTable[2][0], i);
+		WaterTable[2][i].abs = 0;
+
+		WaterTable[3][i].shimmer = (96 * sin) >> 15;
+		WaterTable[3][i].choppy = 0;
+		WaterTable[3][i].random = (uchar)GetRandom(&WaterTable[3][0], i);
+		WaterTable[3][i].abs = 4;
+
+		WaterTable[4][i].shimmer = (127 * sin) >> 15;
+		WaterTable[4][i].choppy = 0;
+		WaterTable[4][i].random = (uchar)GetRandom(&WaterTable[4][0], i);
+		WaterTable[4][i].abs = 8;
+
+		for (int j = 0, k = 5; j < 4; j++, k += 4)
+		{
+			for (int m = 0; m < 4; m++)
+			{
+				WaterTable[k + m][i].shimmer = -((sin * water_shimmer[m]) >> 15);
+				WaterTable[k + m][i].choppy = sin * water_choppy[j] >> 12;
+				WaterTable[k + m][i].random = (uchar)GetRandom(&WaterTable[k + m][0], i);
+				WaterTable[k + m][i].abs = water_abs[m];
+			}
+		}
+	}
+}
+
 void inject_init(bool replace)
 {
 	INJECT(0x00485EA0, ShutdownGame, replace);
 	INJECT(0x00486050, CalculateWibbleTable, replace);
+	INJECT(0x00485CA0, GetRandom, replace);
+	INJECT(0x00485AB0, init_water_table, replace);
 }
--- tomb3decomp/specific/init.h
+++ tomb3decomp/specific/init.h
@@ -5,8 +5,9 @@ void inject_init(bool replace);
 
 void ShutdownGame();
 void CalculateWibbleTable();
+ushort GetRandom(WATERTAB* wt, long lp);
+void init_water_table();
 
 #define init_game_malloc	( (void(__cdecl*)()) 0x00485F60 )
 #define game_malloc	( (void*(__cdecl*)(long, long)) 0x00485F90 )
 #define game_free	( (void(__cdecl*)(long, long)) 0x00486010 )
-#define init_water_table	( (void(__cdecl*)()) 0x00485AB0 )
