2572100b56a00c6c298b8196c63cdb238c761a25
Troye <woopstombraider@gmail.com>
2022-10-21 22:40:48 -0500

    DI_ReadKeyboard

--- tomb3decomp/specific/di.cpp
+++ tomb3decomp/specific/di.cpp
@@ -1,2 +1,22 @@
 #include "../tomb3/pch.h"
 #include "di.h"
+
+#define Keyboard	VAR_(0x006302A8, LPDIRECTINPUTDEVICE)
+
+void DI_ReadKeyboard(uchar* KeyMap)
+{
+	HRESULT state;
+
+	state = Keyboard->GetDeviceState(256, KeyMap);
+
+	if (FAILED(state) && state == DIERR_INPUTLOST)
+	{
+		if (FAILED(Keyboard->Acquire()) || FAILED(Keyboard->GetDeviceState(256, KeyMap)))
+			memset(KeyMap, 0, 256);
+	}
+}
+
+void inject_di(bool replace)
+{
+	INJECT(0x00475450, DI_ReadKeyboard, replace);
+}
--- tomb3decomp/specific/di.h
+++ tomb3decomp/specific/di.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "../global/vars.h"
 
-#define DI_ReadKeyboard	( (void(__cdecl*)(uchar*)) 0x00475450 )
+void inject_di(bool replace);
+
+void DI_ReadKeyboard(uchar* KeyMap);
+
 #define DI_ReadJoystick	( (long(__cdecl*)(long&, long&)) 0x004754B0 )
 #define DI_Finish	( (void(__cdecl*)()) 0x004756F0 )
