7d7030d9225ca941253f879427c5ea0d05bdfbe3
Troye <woopstombraider@gmail.com>
2022-10-22 03:27:34 -0500

    DI_ReadJoystick

--- tomb3decomp/specific/di.cpp
+++ tomb3decomp/specific/di.cpp
@@ -16,7 +16,49 @@ void DI_ReadKeyboard(uchar* KeyMap)
 	}
 }
 
+long DI_ReadJoystick(long& x, long& y)
+{
+	JOYINFOEX joystick;
+	static JOYCAPS caps;
+	static long unavailable = 1;
+
+	if (!App.DXConfig.Joystick)
+	{
+		x = 0;
+		y = 0;
+		return 0;
+	}
+
+	joystick.dwSize = sizeof(JOYINFOEX);
+	joystick.dwFlags = JOY_RETURNX | JOY_RETURNY | JOY_RETURNBUTTONS;
+
+	if (joyGetPosEx(0, &joystick) != JOYERR_NOERROR)
+	{
+		unavailable = 1;
+		x = 0;
+		y = 0;
+		return 0;
+	}
+
+	if (unavailable)
+	{
+		if (joyGetDevCaps(JOYSTICKID1, &caps, sizeof(caps)) != JOYERR_NOERROR)
+		{
+			x = 0;
+			y = 0;
+			return 0;
+		}
+		else
+			unavailable = 0;
+	}
+
+	x = (joystick.dwXpos << 5) / (caps.wXmax - caps.wXmin) - 16;
+	y = (joystick.dwYpos << 5) / (caps.wYmax - caps.wYmin) - 16;
+	return joystick.dwButtons;
+}
+
 void inject_di(bool replace)
 {
 	INJECT(0x00475450, DI_ReadKeyboard, replace);
+	INJECT(0x004754B0, DI_ReadJoystick, replace);
 }
--- tomb3decomp/specific/di.h
+++ tomb3decomp/specific/di.h
@@ -4,6 +4,6 @@
 void inject_di(bool replace);
 
 void DI_ReadKeyboard(uchar* KeyMap);
+long DI_ReadJoystick(long& x, long& y);
 
-#define DI_ReadJoystick	( (long(__cdecl*)(long&, long&)) 0x004754B0 )
 #define DI_Finish	( (void(__cdecl*)()) 0x004756F0 )
