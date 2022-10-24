1aeddab5063efe2005040d611f8805a6fab3ac2e
Troye <woopstombraider@gmail.com>
2022-10-23 21:45:28 -0500

    CalcVolume; S_SoundSetMasterVolume;

--- tomb3decomp/specific/specific.cpp
+++ tomb3decomp/specific/specific.cpp
@@ -3,6 +3,8 @@
 #include "file.h"
 #include "utils.h"
 
+static long MasterVolume;
+
 uchar SWR_FindNearestPaletteEntry(uchar* p, long r, long g, long b, bool ignoreSystemPalette)
 {
 	double best, dr, dg, db, d;
@@ -125,8 +127,20 @@ bool CD_Init()
 	return 1;
 }
 
+long CalcVolume(long volume)
+{
+	return long((float(MasterVolume * volume) * 0.00019074068F - 400.0F) * 6.0F);
+}
+
+void S_SoundSetMasterVolume(long volume)
+{
+	MasterVolume = volume;
+}
+
 void inject_specific(bool replace)
 {
 	INJECT(0x0048D500, SWR_FindNearestPaletteEntry, replace);
 	INJECT(0x0048D2E0, CD_Init, replace);
+	INJECT(0x0048D0A0, CalcVolume, replace);
+	INJECT(0x0048D200, S_SoundSetMasterVolume, replace);
 }
--- tomb3decomp/specific/specific.h
+++ tomb3decomp/specific/specific.h
@@ -5,11 +5,12 @@ void inject_specific(bool replace);
 
 uchar SWR_FindNearestPaletteEntry(uchar* p, long r, long g, long b, bool ignoreSystemPalette);
 bool CD_Init();
+long CalcVolume(long volume);
+void S_SoundSetMasterVolume(long volume);
 
 #define S_CDStop	( (void(__cdecl*)()) 0x0048D4A0 )
 #define S_CDPlay	( (void(__cdecl*)(short, long)) 0x0048D480 )
 #define S_CDVolume	( (void(__cdecl*)(long)) 0x0048D4F0 )
-#define S_SoundSetMasterVolume	( (void(__cdecl*)(long)) 0x0048D200 )
 #define S_CDLoop	( (void(__cdecl*)()) 0x0048D260 )
 #define S_StartSyncedAudio	( (long(__cdecl*)(long)) 0x0048D4B0 )
 #define S_CDGetLoc	( (long(__cdecl*)()) 0x0048D4E0 )
