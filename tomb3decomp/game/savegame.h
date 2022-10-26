cec9fe60f1bad9137d1726be110a0d82bca6b834
Troye <woopstombraider@gmail.com>
2022-10-26 05:13:08 -0500

    CreateStartInfo

--- tomb3decomp/game/savegame.h
+++ tomb3decomp/game/savegame.h
@@ -5,6 +5,6 @@ void inject_savegame(bool replace);
 
 void ModifyStartInfo(long level);
 void InitialiseStartInfo();
+void CreateStartInfo(long level);
 
 #define CreateSaveGameInfo	( (void(__cdecl*)()) 0x00461DD0 )
-#define CreateStartInfo	( (void(__cdecl*)(long)) 0x00461B50 )
