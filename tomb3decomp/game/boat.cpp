2186ef0114c3d7d700bfa98c48851f2b630648ba
Troye <woopstombraider@gmail.com>
2022-10-27 17:14:10 -0500

    BoatCheckGeton

--- tomb3decomp/game/boat.cpp
+++ tomb3decomp/game/boat.cpp
@@ -1,6 +1,9 @@
 #include "../tomb3/pch.h"
 #include "boat.h"
 #include "../specific/init.h"
+#include "../3dsystem/phd_math.h"
+#include "collide.h"
+#include "sphere.h"
 
 void InitialiseBoat(short item_number)
 {
@@ -25,7 +28,70 @@ void InitialiseBoat(short item_number)
 	}
 }
 
+static long BoatCheckGeton(short item_number, COLL_INFO* coll)
+{
+	ITEM_INFO* item;
+	long dx, dz, pass;
+	short ang;
+
+	if (lara.gun_status != LG_ARMLESS)
+		return 0;
+
+	item = &items[item_number];
+	dx = lara_item->pos.x_pos - item->pos.x_pos;
+	dz = lara_item->pos.z_pos - item->pos.z_pos;
+
+	if ((dz * phd_cos(-item->pos.y_rot) - dx * phd_sin(-item->pos.y_rot)) >> W2V_SHIFT > 512)
+		return 0;
+
+	pass = 0;
+	ang = item->pos.y_rot - lara_item->pos.y_rot;
+
+	if (lara.water_status == LARA_SURFACE || lara.water_status == LARA_WADE)
+	{
+		if (!(input & IN_ACTION) || lara_item->gravity_status || item->speed)
+			return 0;
+
+		if (ang > 0x2000 && ang < 0x6000)
+			pass = 1;
+		else if (ang > -0x6000 && ang < -0x2000)
+			pass = 2;
+	}
+	else if (lara.water_status == LARA_ABOVEWATER)
+	{
+		if (lara_item->fallspeed > 0)
+		{
+			if (lara_item->pos.y_pos + 512 > item->pos.y_pos)
+				pass = 3;
+		}
+		else if (!lara_item->fallspeed)
+		{
+			if (ang > -0x6000 && ang < 0x6000)
+			{
+				if (lara_item->pos.x_pos == item->pos.x_pos &&
+					lara_item->pos.y_pos == item->pos.y_pos &&
+					lara_item->pos.z_pos == item->pos.z_pos)
+					pass = 4;
+				else
+					pass = 3;
+			}
+		}
+	}
+
+	if (pass)
+	{
+		if (!TestBoundsCollide(item, lara_item, coll->radius))
+			return 0;
+
+		if (!TestCollision(item, lara_item))
+			return 0;
+	}
+
+	return pass;
+}
+
 void inject_boat(bool replace)
 {
 	INJECT(0x00411FE0, InitialiseBoat, replace);
+	INJECT(0x00412040, BoatCheckGeton, replace);
 }
