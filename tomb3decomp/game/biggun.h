94ca118583520f9ee845dcc3c2e0f5abcaeaf458
Troye <woopstombraider@gmail.com>
2022-11-06 06:19:42 -0600

    BIGGUN_STATE

--- tomb3decomp/game/biggun.h
+++ tomb3decomp/game/biggun.h
@@ -7,3 +7,11 @@ void BigGunInitialise(short item_number);
 void BigGunCollision(short item_number, ITEM_INFO* l, COLL_INFO* coll);
 void BigGunDraw(ITEM_INFO* item);
 long BigGunControl(COLL_INFO* coll);
+
+enum BIGGUN_STATE
+{
+	BGUNS_GETON,
+	BGUNS_GETOFF,
+	BGUNS_UPDOWN,
+	BGUNS_RECOIL
+};
