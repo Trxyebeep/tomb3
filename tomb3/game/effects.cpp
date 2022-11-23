40bd5b4767a61c5158d8c15cad4e6d8397a53430
Troye <woopstombraider@gmail.com>
2022-11-22 19:05:30 -0600

    ItemNearLara

--- tomb3/game/effects.cpp
+++ tomb3/game/effects.cpp
@@ -3,6 +3,7 @@
 #include "lara.h"
 #include "../specific/game.h"
 #include "effect2.h"
+#include "draw.h"
 
 void LaraBreath(ITEM_INFO* item)
 {
@@ -38,7 +39,28 @@ void LaraBreath(ITEM_INFO* item)
 	TriggerBreath(p.x, p.y, p.z, v.x - p.x, v.y - p.y, v.z - p.z);
 }
 
+long ItemNearLara(PHD_3DPOS* pos, long rad)
+{
+	short* bounds;
+	long dx, dy, dz;
+
+	dx = pos->x_pos - lara_item->pos.x_pos;
+	dy = pos->y_pos - lara_item->pos.y_pos;
+	dz = pos->z_pos - lara_item->pos.z_pos;
+
+	if (dx >= -rad && dx <= rad && dz >= -rad && dz <= rad && dy >= -3072 && dy <= 3072 && SQUARE(dx) + SQUARE(dz) <= SQUARE(rad))
+	{
+		bounds = GetBoundsAccurate(lara_item);
+
+		if (dy >= bounds[2] && dy <= bounds[3] + 100)
+			return 1;
+	}
+
+	return 0;
+}
+
 void inject_effects(bool replace)
 {
 	INJECT(0x0042E630, LaraBreath, replace);
+	INJECT(0x0042E170, ItemNearLara, replace);
 }
--- tomb3/game/effects.h
+++ tomb3/game/effects.h
@@ -4,6 +4,7 @@
 void inject_effects(bool replace);
 
 void LaraBreath(ITEM_INFO* item);
+long ItemNearLara(PHD_3DPOS* pos, long rad);
 
 #define effect_routines (*(void(__cdecl*(*)[60])(ITEM_INFO*)) 0x004C5478)
 
@@ -13,5 +14,4 @@ void LaraBreath(ITEM_INFO* item);
 #define DoLotsOfBlood	( (void(__cdecl*)(long, long, long, short, short, short, long)) 0x0042E3B0 )
 #define DoLotsOfBloodD	( (void(__cdecl*)(long, long, long, short, short, short, long)) 0x0042E460 )
 #define SoundEffects	( (void(__cdecl*)()) 0x0042E200 )
-#define ItemNearLara	( (long(__cdecl*)(PHD_3DPOS*, long)) 0x0042E170 )
 #define Richochet	( (void(__cdecl*)(GAME_VECTOR*)) 0x0042E270 )
