0354e065f19eeaf30805ace71335aec6eb8b4c7d
Troye <woopstombraider@gmail.com>
2022-11-24 15:22:20 -0600

    DiverControl

--- tomb3/game/diver.cpp
+++ tomb3/game/diver.cpp
@@ -6,6 +6,9 @@
 #include "objects.h"
 #include "items.h"
 #include "missile.h"
+#include "box.h"
+
+static BITE_INFO diver_poon = { 17, 164, 44, 18 };
 
 long GetWaterSurface(long x, long y, long z, short room_number)
 {
@@ -137,9 +140,142 @@ static short Harpoon(long x, long y, long z, short speed, short ang, short room_
 	return fx_num;
 }
 
+void DiverControl(short item_number)
+{
+	ITEM_INFO* item;
+	CREATURE_INFO* diver;
+	AI_INFO info;
+	GAME_VECTOR start;
+	GAME_VECTOR target;
+	long surface, shoot;
+	short angle;
+
+	if (!CreatureActive(item_number))
+		return;
+
+	item = &items[item_number];
+	diver = (CREATURE_INFO*)item->data;
+
+	if (item->hit_points <= 0)
+	{
+		if (item->current_anim_state != 9)
+		{
+			item->anim_number = objects[DIVER].anim_index + 16;
+			item->frame_number = anims[item->anim_number].frame_base;
+			item->current_anim_state = 9;
+		}
+
+		CreatureFloat(item_number);
+		return;
+	}
+
+	CreatureAIInfo(item, &info);
+	GetCreatureMood(item, &info, 0);
+	CreatureMood(item, &info, 0);
+
+	if (lara.water_status == LARA_ABOVEWATER)
+	{
+		start.x = item->pos.x_pos;
+		start.y = item->pos.y_pos - 256;
+		start.z = item->pos.z_pos;
+		start.room_number = item->room_number;
+		target.x = lara_item->pos.x_pos;
+		target.y = lara_item->pos.y_pos - 612;
+		target.z = lara_item->pos.z_pos;
+		shoot = LOS(&start, &target);
+
+		if (shoot)
+		{
+			diver->target.x = lara_item->pos.x_pos;
+			diver->target.y = lara_item->pos.y_pos;
+			diver->target.z = lara_item->pos.z_pos;
+		}
+
+		if (info.angle < -0x2000 || info.angle > 0x2000)
+			shoot = 0;
+	}
+	else if (info.angle > -0x2000 && info.angle < 0x2000)
+	{
+		start.x = item->pos.x_pos;
+		start.y = item->pos.y_pos;
+		start.z = item->pos.z_pos;
+		start.room_number = item->room_number;
+		target.x = lara_item->pos.x_pos;
+		target.y = lara_item->pos.y_pos;
+		target.z = lara_item->pos.z_pos;
+		shoot = LOS(&start, &target);
+	}
+	else
+		shoot = 0;
+
+	angle = CreatureTurn(item, diver->maximum_turn);
+	surface = GetWaterSurface(item->pos.x_pos, item->pos.y_pos, item->pos.z_pos, item->room_number) + 512;
+
+	switch (item->current_anim_state)
+	{
+	case 1:
+		diver->maximum_turn = 546;
+
+		if (diver->target.y < surface && item->pos.y_pos < surface + diver->LOT.fly)
+			item->goal_anim_state = 2;
+		else if (diver->mood != ESCAPE_MOOD && shoot)
+			item->goal_anim_state = 4;
+
+		break;
+
+	case 2:
+		diver->maximum_turn = 546;
+
+		if (diver->target.y > surface)
+			item->goal_anim_state = 1;
+		else if (diver->mood != ESCAPE_MOOD && shoot)
+			item->goal_anim_state = 6;
+
+		break;
+
+	case 3:
+	case 7:
+
+		if (!diver->flags)
+		{
+			CreatureEffect(item, &diver_poon, Harpoon);
+			diver->flags = 1;
+		}
+
+		break;
+
+	case 4:
+		diver->flags = 0;
+
+		if (!shoot || diver->mood == ESCAPE_MOOD || diver->target.y < surface && item->pos.y_pos < surface + diver->LOT.fly)
+			item->goal_anim_state = 1;
+		else
+			item->goal_anim_state = 3;
+
+		break;
+
+	case 6:
+		diver->flags = 0;
+
+		if (!shoot || diver->mood == ESCAPE_MOOD || diver->target.y > surface)
+			item->goal_anim_state = 2;
+		else
+			item->goal_anim_state = 7;
+		break;
+	}
+
+	CreatureAnimation(item_number, angle, 0);
+
+	if (item->current_anim_state == 1 || item->current_anim_state > 2 && item->current_anim_state <= 4)
+		CreatureUnderwater(item, 512);
+	else
+		item->pos.y_pos = surface - 512;
+}
+
 void inject_diver(bool replace)
 {
 	INJECT(0x00423D70, GetWaterSurface, replace);
 	INJECT(0x00423E80, ControlGhostGasEmitter, replace);
 	INJECT(0x00423CF0, Harpoon, replace);
+	INJECT(0x004239A0, DiverControl, replace);
 }
--- tomb3/game/diver.h
+++ tomb3/game/diver.h
@@ -5,3 +5,4 @@ void inject_diver(bool replace);
 
 long GetWaterSurface(long x, long y, long z, short room_number);
 void ControlGhostGasEmitter(short item_number);
+void DiverControl(short item_number);
