e7baa631092eba743a83690095284cf4c5fdfccc
Troye <woopstombraider@gmail.com>
2022-12-04 02:04:43 -0600

    InitialiseNonLotAI

--- tomb3/game/lot.cpp
+++ tomb3/game/lot.cpp
@@ -1,6 +1,7 @@
 #include "../tomb3/pch.h"
 #include "lot.h"
 #include "../specific/init.h"
+#include "objects.h"
 
 void InitialiseLOTarray()
 {
@@ -28,7 +29,72 @@ void InitialiseLOTarray()
 	nonlot_slots_used = 0;
 }
 
+void InitialiseNonLotAI(short item_number, long slot)
+{
+	ITEM_INFO* item;
+	CREATURE_INFO* creature;
+
+	item = &items[item_number];
+	creature = &non_lot_slots[slot];
+
+	if (item_number == lara.item_number)
+		lara.creature = &non_lot_slots[slot];
+	else
+		item->data = creature;
+
+	creature->item_num = item_number;
+	creature->mood = BORED_MOOD;
+	creature->joint_rotation[0] = 0;
+	creature->joint_rotation[1] = 0;
+	creature->joint_rotation[2] = 0;
+	creature->joint_rotation[3] = 0;
+	creature->alerted = 0;
+	creature->head_left = 0;
+	creature->head_right = 0;
+	creature->reached_goal = 0;
+	creature->hurt_by_lara = 0;
+	creature->patrol2 = 0;
+	creature->maximum_turn = 182;
+	creature->flags = 0;
+	creature->enemy = 0;
+	creature->LOT.step = 256;
+	creature->LOT.drop = -512;
+	creature->LOT.block_mask = 0x4000;
+	creature->LOT.fly = 0;
+
+	switch (item->object_number)
+	{
+	case LARA:
+		creature->LOT.step = 20480;
+		creature->LOT.drop = -20480;
+		creature->LOT.fly = 256;
+		break;
+
+	case WHALE:
+	case DIVER:
+	case CROW:
+	case VULTURE:
+		creature->LOT.step = 20480;
+		creature->LOT.drop = -20480;
+		creature->LOT.fly = 16;
+
+		if (item->object_number == WHALE)
+			creature->LOT.block_mask = 0x8000;
+
+		break;
+
+	case LIZARD_MAN:
+	case MP1:
+		creature->LOT.step = 1024;
+		creature->LOT.drop = -1024;
+		break;
+	}
+
+	nonlot_slots_used++;
+}
+
 void inject_lot(bool replace)
 {
 	INJECT(0x00452F10, InitialiseLOTarray, replace);
+	INJECT(0x00453740, InitialiseNonLotAI, replace);
 }
--- tomb3/game/lot.h
+++ tomb3/game/lot.h
@@ -4,6 +4,7 @@
 void inject_lot(bool replace);
 
 void InitialiseLOTarray();
+void InitialiseNonLotAI(short item_number, long slot);
 
 #define DisableBaddieAI	( (void(__cdecl*)(short)) 0x00452F90 )
 #define EnableBaddieAI	( (long(__cdecl*)(short, long)) 0x00453000 )
