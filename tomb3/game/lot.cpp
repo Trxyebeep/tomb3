ee78178bf7d7b0843fb25bdfc1c84f3b63425443
Troye <woopstombraider@gmail.com>
2022-12-04 20:16:18 -0600

    InitialiseSlot

--- tomb3/game/lot.cpp
+++ tomb3/game/lot.cpp
@@ -252,6 +252,92 @@ void CreateZone(ITEM_INFO* item)
 	}
 }
 
+void InitialiseSlot(short item_number, long slot)
+{
+	ITEM_INFO* item;
+	CREATURE_INFO* creature;
+
+	item = &items[item_number];
+	creature = &baddie_slots[slot];
+
+	if (item_number == lara.item_number)
+		lara.creature = &baddie_slots[slot];
+	else
+		item->data = creature;
+
+	creature->item_num = item_number;
+	creature->mood = BORED_MOOD;
+	creature->joint_rotation[0] = 0;
+	creature->joint_rotation[1] = 0;
+	creature->joint_rotation[2] = 0;
+	creature->joint_rotation[3] = 0;
+	creature->alerted = 0;
+	creature->head_left = 0;
+	creature->head_right = 0;
+	creature->reached_goal = 0;
+	creature->hurt_by_lara = 0;
+	creature->patrol2 = 0;
+	creature->maximum_turn = 182;
+	creature->flags = 0;
+	creature->enemy = 0;
+	creature->LOT.step = 256;
+	creature->LOT.drop = -512;
+	creature->LOT.block_mask = 0x4000;
+	creature->LOT.fly = 0;
+
+	switch (item->object_number)
+	{
+	case LARA:
+		creature->LOT.step = 20480;
+		creature->LOT.drop = -20480;
+		creature->LOT.fly = 256;
+		break;
+
+	case WHALE:
+	case DIVER:
+	case CROW:
+	case VULTURE:
+	case CROCODILE:
+	case MUTANT1:
+		creature->LOT.step = 20480;
+		creature->LOT.drop = -20480;
+		creature->LOT.fly = 16;
+
+		if (item->object_number == WHALE)
+			creature->LOT.block_mask = 0x8000;
+
+		break;
+
+	case LIZARD_MAN:
+	case WILLARD_BOSS:
+	case PUNK1:
+	case MP1:
+	case BOB:
+	case CIVVIE:
+	case MONKEY:
+		creature->LOT.step = 1024;
+		creature->LOT.drop = -1024;
+		break;
+
+	case LON_BOSS:
+		creature->LOT.step = 1024;
+		creature->LOT.drop = -768;
+		break;
+
+	case SHIVA:
+	case TREX:
+		creature->LOT.block_mask = 0x8000;
+		break;
+	}
+
+	ClearLOT(&creature->LOT);
+
+	if (item_number != lara.item_number)
+		CreateZone(item);
+
+	slots_used++;
+}
+
 void inject_lot(bool replace)
 {
 	INJECT(0x00452F10, InitialiseLOTarray, replace);
@@ -260,4 +346,5 @@ void inject_lot(bool replace)
 	INJECT(0x00452F90, DisableBaddieAI, replace);
 	INJECT(0x00453560, ClearLOT, replace);
 	INJECT(0x00453460, CreateZone, replace);
+	INJECT(0x004531F0, InitialiseSlot, replace);
 }
--- tomb3/game/lot.h
+++ tomb3/game/lot.h
@@ -9,5 +9,6 @@ long EnableNonLotAI(short item_number, long Always);
 void DisableBaddieAI(short item_number);
 void ClearLOT(LOT_INFO* lot);
 void CreateZone(ITEM_INFO* item);
+void InitialiseSlot(short item_number, long slot);
 
 #define EnableBaddieAI	( (long(__cdecl*)(short, long)) 0x00453000 )
