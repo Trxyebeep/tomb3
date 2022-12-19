59998e78861023c2a532bf6bf701fc09f8f8f1a0
Troye <woopstombraider@gmail.com>
2022-12-19 11:36:24 -0600

    WinstonControl

--- tomb3/game/winston.h
+++ tomb3/game/winston.h
@@ -4,3 +4,22 @@
 void inject_winston(bool replace);
 
 void OldWinstonControl(short item_number);
+void WinstonControl(short item_number);
+
+enum WINSTON_STATES
+{
+	WINSTON_EMPTY,
+	WINSTON_STOP,
+	WINSTON_WALK,
+	WINSTON_DEF1,
+	WINSTON_DEF2,
+	WINSTON_DEF3,
+	WINSTON_HIT1,
+	WINSTON_HIT2,
+	WINSTON_HIT3,
+	WINSTON_HITDOWN,
+	WINSTON_FALLDOWN,
+	WINSTON_GETUP,
+	WINSTON_BRUSHOFF,
+	WINSTON_ONFLOOR
+};
