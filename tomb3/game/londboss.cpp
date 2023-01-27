ef47e1d1acfb34489dc2432375c334d2a7495e4e
Troye <woopstombraider@gmail.com>
2023-01-26 18:47:11 -0600

    londboss TriggerPlasmaBallFlame

--- tomb3/game/londboss.cpp
+++ tomb3/game/londboss.cpp
@@ -30,6 +30,68 @@ static void TriggerPlasmaBall(ITEM_INFO* item, long type, PHD_VECTOR* pos, short
 	}
 }
 
+static void TriggerPlasmaBallFlame(short fx_number, long type, long xv, long yv, long zv)
+{
+	FX_INFO* fx;
+	SPARKS* sptr;
+	long dx, dz;
+
+	fx = &effects[fx_number];
+	dx = lara_item->pos.x_pos - fx->pos.x_pos;
+	dz = lara_item->pos.z_pos - fx->pos.z_pos;
+
+	if (dx < -0x4000 || dx > 0x4000 || dz < -0x4000 || dz > 0x4000)
+		return;
+
+	sptr = &sparks[GetFreeSpark()];
+	sptr->On = 1;
+	sptr->sR = 48;
+	sptr->sG = 255;
+	sptr->sB = (GetRandomControl() & 0x1F) + 48;
+	sptr->dR = 32;
+	sptr->dG = (GetRandomControl() & 0x3F) + 192;
+	sptr->dB = (GetRandomControl() & 0x3F) + 128;
+	sptr->FadeToBlack = 8;
+	sptr->ColFadeSpeed = (GetRandomControl() & 3) + 12;
+	sptr->TransType = 2;
+	sptr->Life = (GetRandomControl() & 7) + 24;
+	sptr->sLife = sptr->Life;
+	sptr->extras = 0;
+	sptr->Dynamic = -1;
+	sptr->x = (GetRandomControl() & 0xF) - 8;
+	sptr->y = 0;
+	sptr->z = (GetRandomControl() & 0xF) - 8;
+	sptr->Xvel = short((GetRandomControl() & 0xFF) + xv - 128);
+	sptr->Yvel = (short)yv;
+	sptr->Zvel = short((GetRandomControl() & 0xFF) + zv - 128);
+	sptr->Friction = 5;
+	
+	if (GetRandomControl() & 1)
+	{
+		sptr->Flags = SF_ALTDEF | SF_FX | SF_ROTATE | SF_DEF | SF_SCALE;
+		sptr->RotAng = GetRandomControl() & 0xFFF;
+
+		if (GetRandomControl() & 1)
+			sptr->RotAdd = -16 - (GetRandomControl() & 0xF);
+		else
+			sptr->RotAdd = (GetRandomControl() & 0xF) + 16;
+	}
+	else
+		sptr->Flags = SF_ALTDEF | SF_FX | SF_DEF | SF_SCALE;
+
+	sptr->FxObj = (uchar)fx_number;
+	sptr->Def = (uchar)objects[EXPLOSION1].mesh_index;
+	sptr->Scalar = 1;
+	sptr->MaxYvel = 0;
+	sptr->Gravity = 0;
+	sptr->Width = (GetRandomControl() & 0x1F) + 64;
+	sptr->sWidth = sptr->Width;
+	sptr->dWidth = sptr->Width >> 2;
+	sptr->Height = sptr->Width;
+	sptr->sHeight = sptr->Height;
+	sptr->dHeight = sptr->Height >> 2;
+}
+
 long KnockBackCollision(EXPLOSION_RING* ring)
 {
 	long dx, dz, dist;
@@ -82,4 +144,5 @@ void inject_londboss(bool replace)
 {
 	INJECT(0x00451DE0, TriggerPlasmaBall, replace);
 	INJECT(0x00452240, KnockBackCollision, replace);
+	INJECT(0x00452090, TriggerPlasmaBallFlame, replace);
 }
