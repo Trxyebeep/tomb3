81f906c6f30cbd3e02a538d8e12b0c6bf6849f7c
asasas9500 <asasas9500@gmail.com>
2023-02-10 19:00:55 -0300

    Add compy

--- tomb3/game/compy.h
+++ tomb3/game/compy.h
@@ -1,2 +1,16 @@
 #pragma once
 #include "../global/vars.h"
+
+void inject_compy(bool replace);
+
+void InitialiseCompy(short item_number);
+void CompyControl(short item_number);
+
+enum COMPY_STATES
+{
+	COMPY_STOP,
+	COMPY_RUN,
+	COMPY_JUMP,
+	COMPY_ATTACK,
+	COMPY_DEATH
+};
