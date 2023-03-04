b6e87fae598ec8cfe8563c43a5ba4561e62a1e75
Troye <woopstombraider@gmail.com>
2023-03-04 03:54:44 -0600

    ShivaControl

--- tomb3/game/shiva.cpp
+++ tomb3/game/shiva.cpp
@@ -6,6 +6,10 @@
 #include "box.h"
 #include "effects.h"
 #include "sound.h"
+#include "lara.h"
+#include "sphere.h"
+#include "../3dsystem/phd_math.h"
+#include "control.h"
 
 static BITE_INFO shiva_right = { 0, 0, 920, 22 };
 static BITE_INFO shiva_left = { 0, 0, 920, 13 };
@@ -133,12 +137,301 @@ void InitialiseShiva(short item_number)
 	{
 		item->anim_number = objects[SHIVA].anim_index + 14;
 		item->frame_number = anims[item->anim_number].frame_base;
-		item->current_anim_state = 4;
-		item->goal_anim_state = 4;
+		item->current_anim_state = SHIVA_START;
+		item->goal_anim_state = SHIVA_START;
 	}
 
 	item->status = ITEM_INACTIVE;
 	item->mesh_bits = 0;
+}
+
+void ShivaControl(short item_number)
+{
+	ITEM_INFO* item;
+	CREATURE_INFO* shiva;
+	FLOOR_INFO* floor;
+	AI_INFO info;
+	PHD_VECTOR pos;
+	static long effect_mesh;
+	long x, y, z;
+	short lara_alive, torso_x, torso_y, head_y, angle, frame, base, rn;
+
+	if (!CreatureActive(item_number))
+		return;
+
+	item = &items[item_number];
+	shiva = (CREATURE_INFO*)item->data;
+	lara_alive = lara_item->hit_points > 0;
+	torso_x = 0;
+	torso_y = 0;
+	head_y = 0;
+	angle = 0;
+	
+	if (item->hit_points <= 0)
+	{
+		if (item->current_anim_state != SHIVA_DEATH)
+		{
+			item->anim_number = objects[item->object_number].anim_index + 22;
+			item->frame_number = anims[item->anim_number].frame_base;
+			item->current_anim_state = SHIVA_DEATH;
+		}
+	}
+	else
+	{
+		CreatureAIInfo(item, &info);
+		GetCreatureMood(item, &info, 1);
+		CreatureMood(item, &info, 1);
+
+		if (shiva->mood == ESCAPE_MOOD)
+		{
+			shiva->target.x = lara_item->pos.x_pos;
+			shiva->target.z = lara_item->pos.z_pos;
+		}
+
+		angle = CreatureTurn(item, shiva->maximum_turn);
+
+		if (item->current_anim_state != SHIVA_START)
+			item->mesh_bits = -1;
+
+		switch (item->current_anim_state)
+		{
+		case SHIVA_WAIT:
+
+			if (info.ahead)
+				head_y = info.angle;
+
+			if (shiva->flags < 0)
+			{
+				shiva->flags++;
+				x = (GetRandomControl() & 0x5FF) + item->pos.x_pos - 768;
+#ifdef TROYESTUFF
+				y = item->pos.y_pos - (GetRandomControl() & 0x5FF);	//typo maybe?
+#else
+				y = pos.y - (GetRandomControl() & 0x5FF);
+#endif
+				z = (GetRandomControl() & 0x5FF) + item->pos.z_pos - 768;
+				TriggerShivaSmoke(x, y, z, 1);
+			}
+			else
+			{
+				if (shiva->flags == 1)
+					shiva->flags = 0;
+
+				shiva->maximum_turn = 0;
+
+				if (shiva->mood == ESCAPE_MOOD)
+				{
+					rn = item->room_number;
+					x = item->pos.x_pos + (WALL_SIZE * phd_sin(item->pos.y_rot + 0x8000) >> W2V_SHIFT);
+					y = item->pos.y_pos;
+					z = item->pos.z_pos + (WALL_SIZE * phd_cos(item->pos.y_rot + 0x8000) >> W2V_SHIFT);
+					floor = GetFloor(x, y, z, &rn);
+
+					if (shiva->flags || floor->box == 0x7FF || boxes[floor->box].overlap_index & 0x8000)
+						item->goal_anim_state = SHIVA_WAIT_DEF;
+					else
+						item->goal_anim_state = SHIVA_WALKBACK;
+				}
+				else if (shiva->mood == BORED_MOOD)
+				{
+					if (GetRandomControl() < 1024)
+						item->goal_anim_state = SHIVA_WALK;
+				}
+				else if (info.bite && info.distance < 0x190000)
+				{
+					item->goal_anim_state = SHIVA_PINCER;
+					shiva->flags = 0;
+				}
+				else if (info.bite && info.distance < 0x1C6E39)
+				{
+					item->goal_anim_state = SHIVA_CHOPPER;
+					shiva->flags = 0;
+				}
+				else if (item->hit_status && info.ahead)
+				{
+					item->goal_anim_state = SHIVA_WAIT_DEF;
+					shiva->flags = 4;
+				}
+				else
+					item->goal_anim_state = SHIVA_WALK;
+			}
+
+			break;
+
+		case SHIVA_WALK:
+
+			if (info.ahead)
+				head_y = info.angle;
+
+			shiva->maximum_turn = 728;
+
+			if (shiva->mood == ESCAPE_MOOD)
+				item->goal_anim_state = SHIVA_WAIT;
+			else if (shiva->mood == BORED_MOOD)
+				item->goal_anim_state = SHIVA_WAIT;
+			else if (info.bite && info.distance < 0x1C6E39)
+			{
+				item->goal_anim_state = SHIVA_WAIT;
+				shiva->flags = 0;
+			}
+			else if (item->hit_status)
+			{
+				item->goal_anim_state = SHIVA_WALK_DEF;
+				shiva->flags = 4;
+			}
+
+			break;
+
+		case SHIVA_WAIT_DEF:
+
+			if (info.ahead)
+				head_y = info.angle;
+
+			shiva->maximum_turn = 0;
+
+			if (item->hit_status || shiva->mood == ESCAPE_MOOD)
+				shiva->flags = 4;
+
+			if ((info.bite && info.distance < 0x1C6E39) || (item->frame_number == anims[item->anim_number].frame_base && !shiva->flags) || !info.ahead)
+			{
+				item->goal_anim_state = SHIVA_WAIT;
+				shiva->flags = 0;
+			}
+			else if (shiva->flags)
+				item->goal_anim_state = SHIVA_WAIT_DEF;
+
+			if (item->frame_number == anims[item->anim_number].frame_base && shiva->flags > 1)
+				shiva->flags -= 2;
+
+			break;
+
+		case SHIVA_WALK_DEF:
+
+			if (info.ahead)
+				head_y = info.angle;
+
+			shiva->maximum_turn = 728;
+
+			if (item->hit_status)
+				shiva->flags = 4;
+
+			if ((info.bite && info.distance < 0x190000) || (item->frame_number == anims[item->anim_number].frame_base && !shiva->flags))
+			{
+				item->goal_anim_state = SHIVA_WALK;
+				shiva->flags = 0;
+			}
+			else if (shiva->flags)
+				item->goal_anim_state = SHIVA_WALK_DEF;
+
+			if (item->frame_number == anims[item->anim_number].frame_base)
+				shiva->flags = 0;
+
+			break;
+
+		case SHIVA_START:
+			shiva->maximum_turn = 0;
+
+			if (shiva->flags)
+				shiva->flags--;
+			else
+			{
+				if (!item->mesh_bits)
+					effect_mesh = 0;
+
+				item->mesh_bits = (item->mesh_bits << 1) + 1;
+				shiva->flags = 1;
+
+				pos.x = 0;
+				pos.y = 0;
+				pos.z = 256;
+				GetJointAbsPosition(item, &pos, effect_mesh++);
+				TriggerExplosionSparks(pos.x, pos.y, pos.z, 2, 0, 0, item->room_number);
+				TriggerShivaSmoke(pos.x, pos.y, pos.z, 1);
+			}
+
+			if (item->mesh_bits == 0x7FFFFFFF)
+			{
+				item->goal_anim_state = SHIVA_WAIT;
+				effect_mesh = 0;
+				shiva->flags = -45;
+			}
+
+			break;
+
+		case SHIVA_PINCER:
+
+			if (info.ahead)
+			{
+				torso_y = info.angle;
+				torso_x = info.x_angle;
+				head_y = info.angle;
+			}
+
+			shiva->maximum_turn = 728;
+			ShivaDamage(item, shiva, 150);
+
+			break;
+
+		case SHIVA_KILL:
+			shiva->maximum_turn = 0;
+			head_y = 0;
+			torso_x = 0;
+			torso_y = 0;
+			frame = item->frame_number;
+			base = anims[item->anim_number].frame_base;
+
+			if (frame == base + 10 || frame == base + 21 || frame == base + 33)
+			{
+				CreatureEffect(item, &shiva_right, DoBloodSplat);
+				CreatureEffect(item, &shiva_left, DoBloodSplat);
+			}
+
+			break;
+
+		case SHIVA_CHOPPER:
+
+			head_y = info.angle;
+			torso_y = info.angle;
+
+			if (info.x_angle > 0)
+				torso_x = info.x_angle;
+
+			shiva->maximum_turn = 728;
+			ShivaDamage(item, shiva, 180);
+
+			break;
+
+		case SHIVA_WALKBACK:
+
+			if (info.ahead)
+				head_y = info.angle;
+
+			shiva->maximum_turn = 728;
+
+			if ((info.ahead && info.distance < 0x1C6E39) || (item->frame_number == anims[item->anim_number].frame_base && !shiva->flags))
+				item->goal_anim_state = SHIVA_WAIT;
+			else if (item->hit_status)
+			{
+				shiva->flags = 4;
+				item->goal_anim_state = SHIVA_WAIT;
+			}
+
+			break;
+		}
+	}
+
+	if (lara_alive && lara_item->hit_points <= 0)
+	{
+		CreatureKill(item, 18, SHIVA_KILL, EXTRA_YETIKILL);
+		return;
+	}
+
+	CreatureTilt(item, 0);
+	CreatureJoint(item, 0, torso_y);
+	CreatureJoint(item, 1, torso_x);
+	CreatureJoint(item, 2, head_y - torso_y);
+	CreatureJoint(item, 3, 0);
+	CreatureAnimation(item_number, angle, 0);
 }
 
 void inject_shiva(bool replace)
@@ -146,4 +439,5 @@ void inject_shiva(bool replace)
 	INJECT(0x00466E00, TriggerShivaSmoke, replace);
 	INJECT(0x00467780, ShivaDamage, replace);
 	INJECT(0x00467010, InitialiseShiva, replace);
+	INJECT(0x00467070, ShivaControl, replace);
 }
--- tomb3/game/shiva.h
+++ tomb3/game/shiva.h
@@ -4,3 +4,18 @@
 void inject_shiva(bool replace);
 
 void InitialiseShiva(short item_number);
+void ShivaControl(short item_number);
+
+enum SHIVA_STATES
+{
+	SHIVA_WAIT,
+	SHIVA_WALK,
+	SHIVA_WAIT_DEF,
+	SHIVA_WALK_DEF,
+	SHIVA_START,
+	SHIVA_PINCER,
+	SHIVA_KILL,
+	SHIVA_CHOPPER,
+	SHIVA_WALKBACK,
+	SHIVA_DEATH
+};
