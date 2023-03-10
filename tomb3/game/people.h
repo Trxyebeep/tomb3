76b8c536948f8e6b50f43cce07dd0082bd01735f
Troye <woopstombraider@gmail.com>
2023-03-10 08:28:02 -0600

    TargetVisible

--- tomb3/game/people.h
+++ tomb3/game/people.h
@@ -7,6 +7,6 @@ short GunShot(long x, long y, long z, short speed, short yrot, short room_number
 short GunHit(long x, long y, long z, short speed, short yrot, short room_number);
 short GunMiss(long x, long y, long z, short speed, short yrot, short room_number);
 long ShotLara(ITEM_INFO* item, AI_INFO* info, BITE_INFO* bite, short extra_rotation, long damage);
+long TargetVisible(ITEM_INFO* item, AI_INFO* info);
 
-#define TargetVisible	( (long(__cdecl*)(ITEM_INFO*, AI_INFO*)) 0x0045B540 )
 #define Targetable	( (long(__cdecl*)(ITEM_INFO*, AI_INFO*)) 0x0045B610 )
