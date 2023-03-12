28c64822c40be35fe32d2ac5b2bdb9a3e8f3db6d
Troye <woopstombraider@gmail.com>
2023-03-11 23:15:00 -0600

    ControlFireHead

--- tomb3/game/firehead.cpp
+++ tomb3/game/firehead.cpp
@@ -3,6 +3,10 @@
 #include "effect2.h"
 #include "../specific/game.h"
 #include "objects.h"
+#include "control.h"
+#include "sphere.h"
+#include "sound.h"
+#include "traps.h"
 
 static void TriggerFireHeadFlame(long x, long y, long z, long angle, long speed)
 {
@@ -70,8 +74,137 @@ void InitialiseFireHead(short item_number)
 	item->goal_anim_state = FIREHEAD_REAR;
 }
 
+void ControlFireHead(short item_number)
+{
+	ITEM_INFO* item;
+	PHD_VECTOR pos;
+	long angle, r, g;
+	long xmin, xmax, ymin, ymax, zmin, zmax, lx, ly, lz;
+
+	item = &items[item_number];
+
+	if (!TriggerActive(item))
+		return;
+
+	if (item->current_anim_state == FIREHEAD_STILL)
+	{
+		item->goal_anim_state = FIREHEAD_REAR;
+		AnimateItem(item);
+		return;
+	}
+
+	angle = (((item->pos.y_rot >> 3) & 0xFFFE) + 0x800) & 0x1FFE;
+	r = (GetRandomControl() & 7) + 24;
+	g = (GetRandomControl() & 3) + 12;
+
+	if (item->current_anim_state == FIREHEAD_REAR)
+	{
+		pos.x = 0;
+		pos.y = 128;
+		pos.z = 0;
+		GetJointAbsPosition(item, &pos, 7);
+
+		item->item_flags[FH_BLOWLOOPS] = item->hit_points;
+		item->item_flags[FH_SPEED] = 0;
+		item->item_flags[FH_STOP] = 0;
+		item->item_flags[FH_DEADLYRANGE] = 0;
+		TriggerDynamic(pos.x, pos.y, pos.z, 8, r, g, 0);
+	}
+	else
+	{
+		//FIREHEAD_BLOW
+		pos.x = 0;
+		pos.y = 0;
+		pos.z = 0;
+		GetJointAbsPosition(item, &pos, 7);
+
+		if (item->item_flags[FH_STOP])
+		{
+			if (item->item_flags[FH_SPEED])
+				item->item_flags[FH_SPEED] -= 64;
+
+			if (item->item_flags[FH_DEADLYRANGE])
+			{
+				item->item_flags[FH_DEADLYRANGE] -= 32;
+
+				if (item->item_flags[FH_DEADLYRANGE] < 0)
+					item->item_flags[FH_DEADLYRANGE] = 0;
+			}
+		}
+		else
+		{
+			SoundEffect(SFX_FLAME_THROWER_LOOP, &item->pos, SFX_DEFAULT);
+
+			if (item->item_flags[FH_SPEED] < 2048)
+				item->item_flags[FH_SPEED] += 64;
+
+			if (item->item_flags[FH_DEADLYRANGE] < 2048)
+				item->item_flags[FH_DEADLYRANGE] += 32;
+		}
+
+		if (item->item_flags[FH_BLOWLOOPS])
+			item->item_flags[FH_BLOWLOOPS]--;
+
+		if (wibble & 4)
+			TriggerFireHeadFlame(pos.x, pos.y, pos.z, angle, item->item_flags[FH_SPEED]);
+
+		if (!item->item_flags[FH_BLOWLOOPS] && !item->item_flags[FH_STOP] && item->frame_number == anims[item->anim_number].frame_base)
+		{
+			item->item_flags[FH_STOP] = 1;
+			item->goal_anim_state = FIREHEAD_REAR;
+		}
+
+		TriggerDynamic(pos.x, pos.y, pos.z, (item->item_flags[1] >> 7) + 8, r, g, 0);
+
+		if (!lara.burn)
+		{
+			ymin = item->pos.y_pos - 1024;
+			ymax = item->pos.y_pos + 256;
+			
+			if (!item->pos.y_rot)
+			{
+				xmin = pos.x;
+				xmax = pos.x + item->item_flags[FH_DEADLYRANGE];
+				zmin = pos.z - 512;
+				zmax = pos.z + 512;
+			}
+			else if (item->pos.y_rot == 0x4000)
+			{
+				xmin = pos.x - 512;
+				xmax = pos.x + 512;
+				zmin = pos.z - item->item_flags[FH_DEADLYRANGE];
+				zmax = pos.z;
+			}
+			else if (item->pos.y_rot == -0x8000)
+			{
+				xmin = pos.x - item->item_flags[FH_DEADLYRANGE];
+				xmax = pos.x;
+				zmin = pos.z - 512;
+				zmax = pos.z + 512;
+			}
+			else
+			{
+				xmin = pos.x - 512;
+				xmax = pos.x + 512;
+				zmin = pos.z;
+				zmax = pos.z + item->item_flags[FH_DEADLYRANGE];
+			}
+
+			lx = lara_item->pos.x_pos;
+			ly = lara_item->pos.y_pos;
+			lz = lara_item->pos.z_pos;
+
+			if (lx > xmin && lx < xmax && ly > ymin && ly < ymax && lz > zmin && lz < zmax)
+				LaraBurn();
+		}
+	}
+
+	AnimateItem(item);
+}
+
 void inject_firehead(bool replace)
 {
 	INJECT(0x0042FE30, TriggerFireHeadFlame, replace);
 	INJECT(0x0042FAF0, InitialiseFireHead, replace);
+	INJECT(0x0042FB50, ControlFireHead, replace);
 }
