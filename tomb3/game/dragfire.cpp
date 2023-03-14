0efc81678523987df7f2222fe2f0a15f467cbcca
Troye <woopstombraider@gmail.com>
2023-03-13 21:57:37 -0500

    TriggerFlamethrowerSmoke

--- tomb3/game/dragfire.cpp
+++ tomb3/game/dragfire.cpp
@@ -16,6 +16,7 @@ static void TriggerFlamethrowerHitFlame(long x, long y, long z)
 		return;
 
 	sptr = &sparks[GetFreeSpark()];
+	sptr->On = 1;
 	sptr->sR = 255;
 	sptr->sG = (GetRandomControl() & 0x1F) + 48;
 	sptr->sB = 48;
@@ -66,7 +67,100 @@ static void TriggerFlamethrowerHitFlame(long x, long y, long z)
 	sptr->dHeight = sptr->Height >> 4;
 }
 
+static void TriggerFlamethrowerSmoke(long x, long y, long z, long uw)
+{
+	SPARKS* sptr;
+	long dx, dz;
+
+	dx = lara_item->pos.x_pos - x;
+	dz = lara_item->pos.z_pos - z;
+
+	if (dx < -0x4000 || dx > 0x4000 || dz < -0x4000 || dz > 0x4000)
+		return;
+
+	sptr = &sparks[GetFreeSpark()];
+	sptr->On = 1;
+
+	if (uw)
+	{
+		sptr->sR = 0;
+		sptr->sG = 0;
+		sptr->sB = 0;
+		sptr->dR = 192;
+		sptr->dG = 192;
+		sptr->dB = 208;
+	}
+	else
+	{
+		sptr->sR = 144;
+		sptr->sG = 144;
+		sptr->sB = 144;
+		sptr->dR = 64;
+		sptr->dG = 64;
+		sptr->dB = 64;
+	}
+
+	sptr->ColFadeSpeed = 8;
+	sptr->FadeToBlack = 23;
+	sptr->Life = (GetRandomControl() & 0xF) + 32;
+	sptr->sLife = sptr->Life;
+
+	if (uw)
+		sptr->TransType = 2;
+	else
+		sptr->TransType = 3;
+
+	sptr->extras = 0;
+	sptr->Dynamic = -1;
+	sptr->x = (GetRandomControl() & 0x1F) + x - 16;
+	sptr->y = (GetRandomControl() & 0x1F) + y - 16;
+	sptr->z = (GetRandomControl() & 0x1F) + z - 16;
+	sptr->Xvel = ((GetRandomControl() & 0xFFF) - 2048) >> 2;
+	sptr->Yvel = (GetRandomControl() & 0xFF) - 128;
+	sptr->Zvel = ((GetRandomControl() & 0xFFF) - 2048) >> 2;
+
+	if (uw)
+	{
+		sptr->Friction = 20;
+		sptr->Yvel >>= 4;
+		sptr->y += 32;
+	}
+	else
+		sptr->Friction = 6;
+
+	sptr->Flags = SF_ALTDEF | SF_ROTATE | SF_DEF | SF_SCALE;
+	sptr->RotAng = GetRandomControl() & 0xFFF;
+
+	if (GetRandomControl() & 1)
+		sptr->RotAdd = -16 - (GetRandomControl() & 0xF);
+	else
+		sptr->RotAdd = (GetRandomControl() & 0xF) + 16;
+
+	sptr->Def = (uchar)objects[EXPLOSION1].mesh_index;
+	sptr->Scalar = 3;
+
+	if (uw)
+	{
+		sptr->MaxYvel = 0;
+		sptr->Gravity = 0;
+	}
+	else
+	{
+		sptr->Gravity = -3 - (GetRandomControl() & 3);
+		sptr->MaxYvel = -4 - (GetRandomControl() & 3);
+	}
+
+	sptr->dWidth = (GetRandomControl() & 0x1F) + 128;
+	sptr->Width = sptr->dWidth >> 2;
+	sptr->sWidth = sptr->Width;
+	
+	sptr->dHeight = sptr->dWidth + (GetRandomControl() & 0x1F) + 32;
+	sptr->Height = sptr->dHeight >> 3;
+	sptr->sHeight = sptr->Height;
+}
+
 void inject_dragfire(bool replace)
 {
 	INJECT(0x00424830, TriggerFlamethrowerHitFlame, replace);
+	INJECT(0x00424A10, TriggerFlamethrowerSmoke, replace);
 }
