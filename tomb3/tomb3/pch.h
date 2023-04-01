d6a2548814454c39d189eb4568794d606e120c93
Troye <woopstombraider@gmail.com>
2023-04-01 14:51:01 -0500

    better pch

--- tomb3/tomb3/pch.h
+++ tomb3/tomb3/pch.h
@@ -7,7 +7,28 @@
 #ifndef PCH_H
 #define PCH_H
 
-// add headers that you want to pre-compile here
-#include "framework.h"
+//#define RANDO_STUFF	1
+#define TROYESTUFF	1
+//#define DO_LOG	1
+
+#define DIRECT3D_VERSION	0x0500
+#define DIRECTDRAW_VERSION	0x0500
+#define DIRECTSOUND_VERSION	0x0500
+#define DIRECTINPUT_VERSION 0x0800
+
+#include <windows.h>
+#if (DIRECT3D_VERSION >= 0x900)
+#include <d3d9.h>
+#else
+#include <d3d.h>
+#endif
+#include <cmath>
+#include <stdio.h>
+#include <dinput.h>
+#include <dsound.h>
+#include <mmreg.h>
+#include <mmeapi.h>
+#include <ctime>
+#include <MSAcm.h>
 
 #endif //PCH_H
